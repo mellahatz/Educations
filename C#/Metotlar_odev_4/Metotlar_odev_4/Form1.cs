93da2a754c1967fd7831d5ccc5c522a6f687fb98
Melahat Zülfikaroğlu <melahat7098@icloud.com>
2022-12-15 16:01:45 +0300

    Metotlar Örnek

---
+++ C#/Metotlar_odev_4/Metotlar_odev_4/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metotlar_odev_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Girilen 2 sayıyı toplayan metot
+
+        int toplam(int s1, int s2)
+        {
+           int toplam = s1 + s2;
+            return toplam;
+                
+        }
+        
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int s1 = Convert.ToInt16(textBox1.Text);
+            int s2 = Convert.ToInt16(textBox2.Text);
+            
+            textBox1.Text = "".ToString();
+            textBox2.Text= "".ToString();
+            label3.Text = toplam(s1, s2).ToString();
+            label1.Text = "sayı 1";
+            label2.Text = "sayı 2";
+
+        }
+    }
+}
